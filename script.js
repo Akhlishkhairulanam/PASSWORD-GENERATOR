1bf4df222bea0f253768f3b4310b6181f3ab0318
Akhlish Khairul Anam <151932454+Akhlishkhairulanam@users.noreply.github.com>
2025-03-24 09:19:22 +0700

    PASSWORD-GENERATOR

---
+++ script.js
@@ -0,0 +1,15 @@
+let passwordLength = document.getElementById("passwordLength")
+let password = document.getElementById("password")
+
+function getPassword(){
+    const lowerAlphabet = "abcdefghijklmnoprstuvwxyz"
+    const upperAlphabet = "ABCDEFGHIJKLMNOPRSTUVWXYZ"
+    const numeric = "0123456789"
+    const simbol = "!@#$%^&*()_+-={}':;?@[],.<>|"
+    
+}
+
+function savePassword(){
+
+}
+
